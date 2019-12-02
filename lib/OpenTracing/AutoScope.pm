2ffc133cc26772bbfc4d28ebca61e101c2ac58f4
Theo van Hoesel <tvanhoesel@perceptyx.com>
2019-12-02 11:33:46 -0800

    use `get_global_tracer`
    $TRACER might be undef, but the `get_global_tracer` will ALWAYS return
    a tracer, either a full implementation, or a NoOp.

--- lib/OpenTracing/AutoScope.pm
+++ lib/OpenTracing/AutoScope.pm
@@ -3,7 +3,7 @@ package OpenTracing::AutoScope;
 use strict;
 use warnings;
 
-use OpenTracing::GlobalTracer qw/$TRACER/;
+use OpenTracing::GlobalTracer;
 
 use Carp;
 use Scope::Context;
@@ -20,7 +20,8 @@ sub start_guarded_span {
     my $operation_name = scalar @_ % 2 ? shift : _context_sub_name( );
     my %options        = @_;
     
-    my $scope = $TRACER->start_active_span( $operation_name, %options );
+    my $scope = OpenTracing::GlobalTracer
+        ->get_global_tracer->start_active_span( $operation_name, %options );
     
     # use a closure, so we can carry over $scope until the end of the scope
     # where this coderef will be 'reaped'
