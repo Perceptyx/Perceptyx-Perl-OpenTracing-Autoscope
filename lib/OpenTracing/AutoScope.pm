94b8293d32987b555da826270177fcae32d65c6a
Theo van Hoesel <tvanhoesel@perceptyx.com>
2019-12-02 11:29:46 -0800

    add a bit off comments for internal purposes

--- lib/OpenTracing/AutoScope.pm
+++ lib/OpenTracing/AutoScope.pm
@@ -9,6 +9,12 @@ use Carp;
 use Scope::Context;
 
 
+# start_guarded_span
+#
+# This class-method will take an optional list of key/value pairs (same as
+# start_active_span) which should be an even sized list.
+# If not, we asume there is a given operation name.
+#
 sub start_guarded_span {
     my $class          = shift;
     my $operation_name = scalar @_ % 2 ? shift : _context_sub_name( );
@@ -16,6 +22,9 @@ sub start_guarded_span {
     
     my $scope = $TRACER->start_active_span( $operation_name, %options );
     
+    # use a closure, so we can carry over $scope until the end of the scope
+    # where this coderef will be 'reaped'
+    #
     Scope::Context->up->reap( sub { $scope->close } );
     
     return
