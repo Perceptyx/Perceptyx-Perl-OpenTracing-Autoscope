c1aec77d7f92475474c6719e53f0379f348ba738
Szymon Nieznański <snieznanski@perceptyx.com>
2021-03-10 18:10:07 +0100

    Check if 'closed' method exists in AutoScope close

--- lib/OpenTracing/AutoScope.pm
+++ lib/OpenTracing/AutoScope.pm
@@ -29,7 +29,9 @@ sub start_guarded_span {
     # use a closure, so we can carry over $scope until the end of the scope
     # where this coderef will be 'reaped'
     #
-    Scope::Context->up->reap( sub { $scope->close unless $scope->closed } );
+    Scope::Context->up->reap(sub {
+        $scope->close unless $scope->can('closed') and $scope->closed
+    });
     
     return
 }
